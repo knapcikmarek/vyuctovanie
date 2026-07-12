e182157b5fec10f0e5e163c7958adb1663fd2d07
Marek Knapčík <knapcik.marek@gmail.com>
2026-07-13 00:16:31 +0200

    uprava min na hodiny

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
 
   const litreSpolu = litreKm + litreMth;
 
-  const MthSpolu = (Number(mth) + Number(pretacanieMin));
+  const MthSpolu = ((Number(mth) / 60) + Number(pretacanieMin));
 
   const zostatok = Number(plnaNadrz) - (((Number(Mthnovymesiac) / 60) * Number(spotrebaMth)) + ((Number(Kmnovymesiac / 100)) * Number(spotreba100)));
 
